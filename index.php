33e78e86352caae411b567a2722e927b0c1e7fb5
jefroni <128974738+jefroni@users.noreply.github.com>
2023-03-26 21:36:42 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Web Pencarian film</title>
+  </head>
+  <body>
+  <h1>Web Pencarian Film</h1>
+    <form method="post" action="index.php">
+        <input type="text" name="judul"></input>
+        <input type="submit" value="Pencarian" name="cari"></input>
+    </form>
+    
+    <?php
+        if(isset($_POST['cari'])){
+            $judul = $_POST['judul'];
+            echo "<h1>Hasil Pencarian</h1>";
+        
+            $url = 'http://www.omdbapi.com/?apikey=202bed7d&s="'.$judul.'"';
+
+           //Akses API dengan CURL
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $output = curl_exec($ch);
+            curl_close($ch);
+
+            // var_dump($output);
+            $data = json_decode($output, TRUE);
+            // $data = $data['Search'];
+            
+            foreach ($data['Search'] as $movie) {
+                
+                echo "\n <p>Judul: ".$movie["Title"]."</p>";
+                echo "<p>Tahun: ".$movie["Year"]."</p>";
+                echo '<img src= "'.$movie['Poster'].'">';
+            }    
+        }
+        ?>
+    
+</body>
+</html>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
